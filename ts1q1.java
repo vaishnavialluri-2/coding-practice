956c6edb977338894c19f8ffebd84d5f8d8d02ec
vaishnavialluri-2 <suryavaishnavialluri@gmail.com>
2026-07-11 11:28:35 +0530

    Reverse an Array In-Place
    Given an array of N integers, reverse the array using only O(1) extra space (do not create a new array).
    Input: First line N. Second line: N space-separated integers.
    Output: The reversed array, space-separated.
    Constraints: 1 <= N <= 10^5, -10^9 <= A[i] <= 10^9

---
+++ ts1q1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class ts1q1 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int first=0,last=n-1;
+        while(first < last ){
+            int temp=arr[first];
+            arr[first]=arr[last];
+            arr[last]=temp;
+            first++;
+            last--;
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]);
+            System.out.print(" ");
+        }
+    }
+}
